ce07cf8d8e91c2a65e2f75c75f7fe125515261b3
kmeda <kathikreddymeda@gmail.com>
2017-01-05 21:37:56 +0530

    Update 2017-01-05T16:07:54.507Z

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,24 @@
 $(function(){
 
-$('.bg-glow').click(function(){
-  $(this).toggleClass('shadow-glow');
-  $('.sessionMask').toggleClass('animateMask');
-  //$('.session').toggleClass('sessionProgBar');
 
-});
+$('.bg-glow').addClass('shadow-glow');
+$('.sessionMask').addClass('sessionProgBar');
+
+
+
+window.randomize = function() {
+		var rotation = Math.floor(Math.random() * 180);
+		var fill_rotation = 180;
+		var fix_rotation = rotation * 2;
+		console.log(rotation);
+
+		$('.session .fill, .session .mask.full').css('-webkit-transform', 'rotate(' + fill_rotation + 'deg)');
+		$('.session .fill.fix').css('-webkit-transform', 'rotate(' + fix_rotation + 'deg)');
+
+
+	}
+	setTimeout(window.randomize, 200);
+	$('.session').click(window.randomize);
+
 
 });
