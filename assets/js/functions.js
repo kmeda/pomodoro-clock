fa2916fc567b648d6e28e7af54b8d0cd4c20d356
kmeda <kathikreddymeda@gmail.com>
2017-01-13 13:50:05 +0530

    timer and animate sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,8 +4,8 @@ $(function(){
 $('.session').css('background-color', '#00FFA7');
 $('.breakTimer').hide();
 
-var sessionInterval = 60
-var breakInterval = 60
+var sessionInterval = 6
+var breakInterval = 6
 
 // Animate Session radial progress
     window.session = function() {
@@ -13,14 +13,11 @@ var breakInterval = 60
                       $('.break, .breakMask').hide();
                       $('.bg-glow').addClass('shadow-glow');
                       $('.maskContainer').addClass('innerShadow');
-
-                      //Radial progress animation
-
                       $('.session .fill, .session .mask.full').css({"animation": "sessionAnimate180"+ " "+(sessionInput*sessionInterval)+"s" +" "+ "linear"+" "+ "forwards"+" "+ "running"+" "+ "1" });
                       $('.session .fill.fix').css({"animation": "sessionAnimate360"+ " "+(sessionInput*sessionInterval)+"s"+" "+"linear"+" "+"forwards" +" "+ "running"+" "+ "1"});
+                      $('.maskContainer').html("<div class='alert'>Session</div>");
 
     };
-
 // Animate Break radial progress
       window.break = function() {
                       $('.break').css('background-color', 'transparent');
@@ -28,6 +25,7 @@ var breakInterval = 60
                       $('.sessionTimer').hide();
                       $('.break .fill, .break .mask.full').css({"animation": "sessionAnimate180"+ " "+(breakInput*breakInterval)+"s" +" "+ "linear"+ " "+"forwards"+" "+ "running"+" "+ "1" });
                       $('.break .fill.fix').css({"animation": "sessionAnimate360"+ " "+(breakInput*breakInterval)+"s"+" "+"linear"+ " "+"forwards" +" "+ "running"+" "+ "1"});
+                      $('.breakMask').append("<div class='alert'>Break</div>");
 
   	};
 
@@ -35,10 +33,6 @@ var breakInterval = 60
   $('.resetAll').click(function(){
     location.reload();
   });
-
-
-
-
 
 // Get Session input
                     var sessionInput = 1;
@@ -72,7 +66,6 @@ var breakInterval = 60
                       }
 
                     });
-
 // Get Break input
                       var breakInput = 1;
                       $('.breakCount').html('0'+breakInput);
@@ -112,12 +105,7 @@ var breakInterval = 60
                         }
 
                       });
-
-
-
-
 //Timer run
-
 var sessionClock = {
   totalSeconds: sessionInput * sessionInterval,
 
@@ -137,9 +125,6 @@ var sessionClock = {
         window.break();
         breakClock.start();
       }
-
-      //$(".min").html(pad(Math.floor(self.totalSeconds / 60 % 60) + ':'));
-      //$(".sec").html(pad(parseInt(self.totalSeconds % 60)));
     }, 1000);
   },
 
@@ -162,18 +147,13 @@ var breakClock = {
     this.interval = setInterval(function () {
       self.totalSeconds -= 1;
 
-      console.log(breakClock.totalSeconds);
-      $(".min").html(pad(Math.floor(self.totalSeconds / 60 % 60) + ':'));
-      $(".sec").html(pad(parseInt(self.totalSeconds % 60)));
+      $(".breakTimer .min").html(pad(Math.floor(self.totalSeconds / 60 % 60) + ':'));
+      $(".breakTimer .sec").html(pad(parseInt(self.totalSeconds % 60)));
 
       if (breakClock.totalSeconds === 0) {
         breakClock.pause();
 
       }
-
-
-      //$(".breakTimer .min").html(pad(Math.floor(self.totalSeconds / 60 % 60) + ':'));
-      //$(".breakTimer .sec").html(pad(parseInt(self.totalSeconds % 60)));
     }, 1000);
   },
 
@@ -187,21 +167,13 @@ var breakClock = {
   }
 };
 
-
-
 //Main Control Click
 $('.maskContainer2').click(function(){
   window.session();
-  //window.break();
   sessionClock.start();
-  //breakClock.start();
   $(this).hide();
   $('.psession, .msession, .pbreak, .mbreak').css('pointer-events', 'none');
 });
-
-//Detect session animation complete event
-
-
 
 });
 
